a64ef96a3701c927d68ea0ea7f14fc52b9178376
Eliab herrera <eliab.herrera_pm25258@cecyte.edu.mx>
2026-05-21 10:12:57 -0600

    se agregaron cambios en el parcial3

---
+++ ejercicios/Eliab-herrera/parcial3/mi_banco/banco.py
@@ -0,0 +1,11 @@
+from cuenta import cuenta
+
+class Banco:
+
+    def trasferir(self, origen, destino, cantidad):
+        if origen.retirar(cantidad):
+            destino.deposito(cantidad)
+            return True
+        return False
+    
+    
